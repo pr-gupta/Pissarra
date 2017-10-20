5636939993ec8fc571a4bcad195d915882f4c00c
pr-gupta <prashantg19051196@gmail.com>
2017-10-21 01:08:53 +0530

    added variable names for tempalte paths

--- src/main/kotlin/co/pissarra/spark/handler/Login.kt
+++ src/main/kotlin/co/pissarra/spark/handler/Login.kt
@@ -4,11 +4,12 @@ import spark.ModelAndView
 import spark.Request
 import spark.Response
 import spark.template.velocity.VelocityTemplateEngine
+import co.pissarra.util.TemplatePath
 
 class Login {
     companion object {
         val getLoginPage = { req: Request, res: Response ->
-            VelocityTemplateEngine().render(ModelAndView(HashMap<String, Any>(), "/public/templates/login.vm"))
+            VelocityTemplateEngine().render(ModelAndView(HashMap<String, Any>(), TemplatePath.GET_LOGIN_PAGE))
         }
     }
 }
---
+++ src/main/kotlin/co/pissarra/util/TemplatePath.kt
@@ -0,0 +1,9 @@
+package co.pissarra.util
+
+class TemplatePath {
+    companion object {
+        private val TEMPLATE_HOME = "/templates"
+
+        val GET_LOGIN_PAGE = TEMPLATE_HOME + "/login.vm"
+    }
+}

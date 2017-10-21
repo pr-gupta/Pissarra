bc69b4d0acf2720f2105d934913e10ddd3a6f295
pr-gupta <prashantg19051196@gmail.com>
2017-10-21 19:47:14 +0530

    corrected the problem with build as the mainclass should be co.pissarra.MainKt and not co.pissarra.Mainkt

--- src/main/kotlin/co/pissarra/Main.java
+++
@@ -1,9 +0,0 @@
-package co.pissarra;
-
-import co.pissarra.util.dagger.component.DaggerAppComponent;
-
-public class Main {
-    public static void main(String[] args) {
-        DaggerAppComponent.builder().build().sparkSetup().init();
-    }
-}
